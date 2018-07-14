5bc997083d92eeedab771b9e90b4d4e7769f842b
dustin <dustinerickson@gmail.com>
2018-07-14 00:27:56 -0700

    init commit signup done

---
+++ server/auth/index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const Joi = require('joi');
+const bcrypt = require('bcryptjs');
+const router = express.Router();
+
+const db = require('../db/connection.js');
+const users = db.get('users');
+users.createIndex('email', { unique: true });
+//any route in this file is prepended with /auth..
+
+//joi validation schema
+const signupSchema = Joi.object().keys({
+    email: Joi.string().email(),
+    password: Joi.string().min(5).max(30).required(),
+});
+
+
+router.get('/', (req, res) => {
+    res.json({
+        message:"pancake is the now world"
+    });
+});
+
+router.post('/signup', (req, res, next) => {
+    const validResult = Joi.validate(req.body, signupSchema);
+    if(validResult.error === null) {
+        console.log(validResult.value.email);
+        users.findOne({email:validResult.value.email}, 'email').then((user)=>{
+            if(user === null) {
+                //\\//\\//\\//hash passwrod here & add user to db here.//||//\\//\\//\\
+                bcrypt.genSalt(10, function(err, salt) {
+                    if(!err){
+                        bcrypt.hash(validResult.value.password, salt, function(errr, hash) {
+                            if(!errr){
+                                // Store hash in your password DB.
+                                users.insert({email:validResult.value.email, password:hash}).then((insertedUser) => {
+                                    res.json(insertedUser);
+                                }).catch((err) => {
+                                    res.status(500);
+                                    next(err);
+                                });
+                                 //respond to client with success
+                            } else {
+                                res.status(500);
+                                next(err);
+                            }
+                        });
+                    } else {
+                        res.status(500);
+                        next(err);
+                    }
+                });
+            } else {
+                const err = new Error("User Already Exists");
+                res.status(400);
+                next(err);
+            }      
+        }).catch((err) => {
+            res.status(500)
+            next(err);
+        });
+    } else {
+        res.status(400);
+        next(validResult.error);
+    }
+});
+
+module.exports = router;
